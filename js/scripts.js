2252ddc0a7230bcce08e6f7dd2fe659324c7b569
leon <leonmonyoro@gmail.com>
2019-04-07 21:12:43 +0300

    Created js code for player 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 $(document).ready(function (){
-   $
-}
+   $("#player1-roll").click => {
+      var random1 = Math.floor((Math.random()*6) + 1);
+      $("#die-roll-1").text("DICE ROLL:" + random1);  
+   });
+});
