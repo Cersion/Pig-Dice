8bb83b9850dfff19713efd7e4b9afdc4779bdb92
leon <leonmonyoro@gmail.com>
2019-04-08 08:07:58 +0300

    Created code for calling the roll and hold function for player1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
-$(document).ready(function (){
-   $("#player1-roll").click => {
-      var random1 = Math.floor((Math.random()*6) + 1);
-      $("#die-roll-1").text("DICE ROLL:" + random1);  
+$(document).ready(function(){
+   $("#begin").click(function(){
+    var nameOne = $("#btnplayer1").val();
+
+    
+    $("#player1h").html(  nameOne);
    });
 });
+
+
+
+/*
+
+$(document).ready(function (){
+   $("#player1-roll").click(function(){
+      var random1 = Math.floor((Math.random()*6) + 1);
+      var initial = 0;
+      var player1 = initial + random1;
+      $("#round-total-1").text("DICE ROLL:" + random1);
+     $("#player1-hold").click(function() {
+        var totalScore =player1;
+        player1 = 0; 
+      $("#total-score-1").text(totalScore)
+     });
+  });
+});
+*/
